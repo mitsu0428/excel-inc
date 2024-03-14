2b0d7a3bcef9b09e6073e393a70c067f8f0b79e8
mitsu0428 <mitsuprograming@gmail.com>
2024-03-14 10:30:27 +0900

    fix: cardはもらったデザインに合わせる

--- src/app/components/ui/NewsCard.tsx
+++ src/app/components/ui/NewsCard.tsx
@@ -6,7 +6,6 @@ import {
   CardContent,
   CardMedia,
   Typography,
-  Button,
   Grid,
   Container,
 } from "@mui/material";
@@ -19,33 +18,29 @@ export const Component = () => {
       thumbnail:
         "https://assets.st-note.com/production/uploads/images/132546569/rectangle_large_type_2_740b08e0d9668ecf0ac7c51e9c421789.png?width=800",
       showDate: "2024.3.4 16:56",
-      desc: "LINE公式アカウントのメリットやデメリット、注意点などをご説明します!!",
       link: "https://note.com/excelinc/n/n6869aa4cf971",
     },
     {
       title: "【ビジネス】知らないと損！Instagramのフォロワーの増やし方",
       thumbnail:
         "https://assets.st-note.com/production/uploads/images/132896657/rectangle_large_type_2_69b446afcc79c01adfe24517ac03462e.jpeg?width=800",
-      showDate: "2024.3.4 16:56",
-      desc: "【ビジネス】知らないと損！Instagramのフォロワーの増やし方",
+      showDate: "2024.3.4 18:55",
       link: "https://note.com/excelinc/n/n6c79b6c242f7",
     },
     {
-      title:
-        "【ビジネス】LINE公式アカウントについてのあれこれ教えちゃいます-その①-",
+      title: "成果を出すために欠かせない！Instagram運用の３つの基本",
       thumbnail:
-        "https://assets.st-note.com/production/uploads/images/132546569/rectangle_large_type_2_740b08e0d9668ecf0ac7c51e9c421789.png?width=800",
-      showDate: "2024.3.4 16:56",
-      desc: "LINE公式アカウントのメリットやデメリット、注意点などをご説明します!!",
-      link: "https://note.com/excelinc/n/n6869aa4cf971",
+        "https://assets.st-note.com/production/uploads/images/133609039/rectangle_large_type_2_3d8969850897041afb6ceb11282e5adc.png?width=600",
+      showDate: "2024.3.12 12:09",
+      link: "https://note.com/excelinc/n/na09abc17861c",
     },
   ];
 
   return (
-    <Container sx={{ padding: "20px" }}>
+    <Container>
       <Grid
         container
-        spacing={8}
+        spacing={3}
       >
         {newsJson.map((news, index) => (
           <Grid
@@ -55,66 +50,35 @@ export const Component = () => {
             md={4}
             key={index}
           >
-            <Card sx={{ height: "500px" }}>
-              <StyledLink
-                href={news.link}
-                target="_blank"
-                passHref
-                key={index}
-              >
+            <StyledLink
+              href={news.link}
+              target="_blank"
+              passHref
+            >
+              <CustomCard>
                 <CardMedia
                   component="img"
-                  height="140"
+                  height="200"
                   image={news.thumbnail}
                   alt={news.title}
                 />
-              </StyledLink>
-
-              <CardContent>
-                <StyledLink
-                  href={news.link}
-                  target="_blank"
-                  passHref
-                  key={index}
-                >
+                <CardContent>
                   <Typography
-                    variant="body2"
-                    color="text.secondary"
-                  >
-                    {news.showDate}
-                  </Typography>
-                </StyledLink>
-
-                <StyledLink
-                  href={news.link}
-                  target="_blank"
-                  passHref
-                  key={index}
-                >
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
+                    variant="h6"
+                    sx={{ fontSize: "14px" }}
                   >
                     {news.title}
                   </Typography>
-                </StyledLink>
 
-                <StyledLink
-                  href={news.link}
-                  target="_blank"
-                  passHref
-                  key={index}
-                >
                   <Typography
                     variant="body2"
-                    color="text.secondary"
+                    sx={{ fontSize: "10px" }}
                   >
-                    {news.desc}
+                    {news.showDate}
                   </Typography>
-                </StyledLink>
-              </CardContent>
-            </Card>
+                </CardContent>
+              </CustomCard>
+            </StyledLink>
           </Grid>
         ))}
       </Grid>
@@ -124,4 +88,10 @@ export const Component = () => {
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+`;
+
+const CustomCard = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 `;
