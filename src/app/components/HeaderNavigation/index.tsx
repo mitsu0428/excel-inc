a00bf2ce0df971103bc1d499b63a196895d6e072
mitsu0428 <mitsuprograming@gmail.com>
2024-03-20 00:45:06 +0900

    fix: menu

--- src/app/components/HeaderNavigation/index.tsx
+++ src/app/components/HeaderNavigation/index.tsx
@@ -9,306 +9,329 @@ import * as Font from "../../styles/NextFont";
 
 export const Component = () => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
+  const toggleMenu = (isOpen: boolean) => {
     setIsOpen(!isOpen);
   };
 
-  const closeMenu = () => {
-    setIsOpen(false);
-  };
+  const menu = [
+    {
+      text: "TOP",
+      link: "/",
+    },
+    {
+      text: "MISSION",
+      link: "/",
+    },
+    {
+      text: "SERVICE",
+      link: "/",
+    },
+    {
+      text: "NEWS",
+      link: "https://note.com/excelinc/",
+    },
+    {
+      text: "COMPANY",
+      link: "#company",
+    },
+    {
+      text: "CONTACT",
+      link: "/contact",
+    },
+  ];
+
+  const snsLink = [
+    {
+      text: "Instagram",
+      link: "/",
+      icon: "/sns-instagram.png",
+    },
+    {
+      text: "TikTok",
+      link: "https://www.tiktok.com/@inforzsb7b?_t=8kOyyHCZkNm&_r=",
+      icon: "/sns-tiktok.png",
+    },
+    {
+      text: "Line",
+      link: "https://line.me/R/ti/p/@595weqhc?oat_content=url",
+      icon: "/sns-line.png",
+    },
+  ];
+
+  const logoLink = "/assets/logo.svg";
+
+  const FOLLOW_US = "FOLLOW US";
 
   return (
     <Wrapper>
-      <RowWrapperWithProps $isOpen={isOpen}>
-        <StyledLink
-          href={"/"}
-          passHref
-        >
-          <StyledImage
-            $isOpen={isOpen}
-            src="/assets/logo.svg"
+      <PC_SCREEN>
+        <RowWrapper>
+          <Logo
+            src={logoLink}
             alt="Excel inc."
-            width={88}
-            height={88}
+            onClick={() => toggleMenu(isOpen)}
           />
-        </StyledLink>
+        </RowWrapper>
 
-        <MenuToggle
-          $isOpen={isOpen}
-          onClick={toggleMenu}
-        >
-          <Line />
-          <Line />
-          <Line />
-        </MenuToggle>
-      </RowWrapperWithProps>
+        <RowWrapper className={Font.Font.CustomJosefinSans.className}>
+          {menu.map((item, index) => (
+            <StyledLink
+              href={item.link}
+              passHref
+              key={index}
+            >
+              <StyledSpan>{item.text}</StyledSpan>
+            </StyledLink>
+          ))}
+        </RowWrapper>
+      </PC_SCREEN>
 
-      <MobileHeadArea $isOpen={isOpen}>
-        <MenuButton onClick={toggleMenu}>
-          <StyledLink
-            href={"/"}
-            passHref
+      <SP_SCREEN>
+        <RowSpaceBetweenWrapper>
+          <Logo
+            src={logoLink}
+            alt="Excel inc."
+          />
+
+          <OpenWrapper
+            $isOpen={isOpen}
+            onClick={() => toggleMenu(isOpen)}
           >
-            <StyledMobileImage
-              $isOpen={isOpen}
-              src="/assets/logo.svg"
+            <OpenOneSpan $isOpen={isOpen} />
+            <OpenTwoSpan $isOpen={isOpen} />
+            <OpenThreeSpan $isOpen={isOpen} />
+          </OpenWrapper>
+        </RowSpaceBetweenWrapper>
+
+        <SP_SCREEN_MENU $isOpen={isOpen}>
+          <RowSpaceBetweenWrapper>
+            <Logo
+              src={logoLink}
               alt="Excel inc."
-              width={88}
-              height={88}
             />
-          </StyledLink>
-        </MenuButton>
-
-        <CloseButton onClick={toggleMenu}>
-          <CrossLine style={{ top: "50%", transform: "rotate(45deg)" }} />
-          <CrossLine style={{ top: "50%", transform: "rotate(-45deg)" }} />
-        </CloseButton>
-      </MobileHeadArea>
-
-      <Menu
-        $isOpen={isOpen}
-        onClick={closeMenu}
-      >
-        <StyledUl $isOpen={isOpen}>
-          <StyledLi className={Font.Font.CustomJosefinSans.className}>
-            <StyledLink
-              href={"/"}
-              passHref
-            >
-              <StyledSpan onClick={closeMenu}>TOP</StyledSpan>
-            </StyledLink>
-          </StyledLi>
-
-          <VerticalBorder />
-
-          <StyledLi className={Font.Font.CustomJosefinSans.className}>
-            <StyledLink
-              href={"/"}
-              passHref
-            >
-              <StyledSpan onClick={closeMenu}>MISSION</StyledSpan>
-            </StyledLink>
-          </StyledLi>
-
-          <VerticalBorder />
-
-          <StyledLi className={Font.Font.CustomJosefinSans.className}>
-            <StyledLink
-              href={"/"}
-              passHref
-            >
-              <StyledSpan onClick={closeMenu}>SERVICE</StyledSpan>
-            </StyledLink>
-          </StyledLi>
-
-          <VerticalBorder />
-
-          <StyledLi className={Font.Font.CustomJosefinSans.className}>
-            <StyledLink
-              target="_blank"
-              href={"https://note.com/excelinc/"}
-              passHref
-            >
-              <StyledSpan onClick={closeMenu}>NEWS</StyledSpan>
-            </StyledLink>
-          </StyledLi>
-
-          <VerticalBorder />
-
-          <StyledLi className={Font.Font.CustomJosefinSans.className}>
-            <StyledLink
-              href={"#company"}
-              passHref
-            >
-              <StyledSpan onClick={closeMenu}>COMPANY</StyledSpan>
-            </StyledLink>
-          </StyledLi>
-
-          <StyledLi className={Font.Font.CustomJosefinSans.className}>
-            <StyledSpecialLink
-              href={"/contact"}
-              passHref
-            >
-              <StyledSpecialSpan onClick={closeMenu}>CONTACT</StyledSpecialSpan>
-            </StyledSpecialLink>
-          </StyledLi>
-
-          <RowWrapper>
-            <FollowUsText
+            <CloseButton
               $isOpen={isOpen}
-              className={Font.Font.CustomNotoSansMyanmar.className}
-            >
-              FOLLOW US
-            </FollowUsText>
+              onClick={() => toggleMenu(isOpen)}
+            />
+          </RowSpaceBetweenWrapper>
 
-            <SnsWrapper $isOpen={isOpen}>
-              <SnsIcon
-                src="/sns-instagram.png"
-                alt="Instagram"
-              />
-              <SnsIcon
-                src="/sns-line.png"
-                alt="Line"
-              />
-              <SnsIcon
-                src="/sns-tiktok.png"
-                alt="TikTok"
-              />
-            </SnsWrapper>
-          </RowWrapper>
-        </StyledUl>
-      </Menu>
+          <ColumnWrapper className={Font.Font.CustomJosefinSans.className}>
+            {menu.map((item, index) => (
+              <RowWrapper key={index}>
+                <StyledLink
+                  href={item.link}
+                  passHref
+                >
+                  <StyledSpan>{item.text}</StyledSpan>
+                </StyledLink>
+              </RowWrapper>
+            ))}
+          </ColumnWrapper>
+
+          <SNSWrapper>
+            <StyledSpan>{FOLLOW_US}</StyledSpan>
+            {snsLink.map((item, index) => (
+              <StyledLink
+                href={item.link}
+                passHref
+                key={index}
+              >
+                <Image
+                  src={item.icon}
+                  alt={item.text}
+                  width={30}
+                  height={30}
+                />
+              </StyledLink>
+            ))}
+          </SNSWrapper>
+        </SP_SCREEN_MENU>
+      </SP_SCREEN>
     </Wrapper>
   );
 };
 
-const RowWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  flex-direction: row;
-`;
+const Wrapper = styled.div``;
 
-const RowWrapperWithProps = styled.div<{ $isOpen: boolean }>`
-  display: ${({ $isOpen }) => ($isOpen ? "none" : "flex")};
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  flex-direction: row;
-`;
-
-const Wrapper = styled.div`
+// memo: PC_SCREENのメニュー
+const PC_SCREEN = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: 84px;
-  padding: 80px 60px;
   position: fixed;
   z-index: 1;
-
   mix-blend-mode: difference;
+  height: 84px;
 
-  @media (max-width: 768px) {
-    mix-blend-mode: normal;
-    height: 71px;
-    padding: 20px 20px;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-`;
-
-const StyledSpecialLink = styled(Link)`
-  text-decoration: none;
-  border: 1px solid #fff;
-  background-color: #fff;
-  padding: 4px 18px;
-`;
-
-const StyledUl = styled.ul<{ $isOpen: boolean }>`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  gap: 20px;
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  color: #fff;
-
-  @media (max-width: 768px) {
-    display: ${({ $isOpen }) => ($isOpen ? "flex" : "none")};
-    flex-direction: column;
-    position: absolute;
-    top: 50px;
-    left: 44px;
-  }
-`;
-
-const StyledLi = styled.li`
-  line-height: normal;
-  font-size: 25px;
-  color: #fff;
-  letter-spacing: 1px;
-  cursor: pointer;
-`;
-
-const StyledMobileImage = styled(Image)<{ $isOpen: boolean }>`
-  @media (max-width: 768px) {
-    display: ${({ $isOpen }) => ($isOpen ? "" : "none")};
-  }
-`;
-
-const StyledImage = styled.img<{ $isOpen: boolean }>`
-  @media (max-width: 768px) {
-    display: ${({ $isOpen }) => ($isOpen ? "none" : "")};
-  }
-`;
-
-const MenuToggle = styled.div<{ $isOpen: boolean }>`
-  display: none;
-
-  @media (max-width: 768px) {
-    display: ${({ $isOpen }) => ($isOpen ? "none" : "flex")};
-    flex-direction: column;
-    cursor: pointer;
-  }
-`;
-
-const Menu = styled.div<{ $isOpen: boolean }>`
-  display: flex;
-  align-items: center;
-
-  @media (max-width: 768px) {
-    background-color: #000;
-    position: fixed;
-    top: 0;
-    right: ${({ $isOpen }) => ($isOpen ? "0" : "-100%")};
-    height: 100vh;
-    width: 100%;
-    transition: all 0.3s ease-in-out;
-    z-index: 2;
-
-    ul {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: left;
-      height: 100%;
-      list-style: none;
-      padding: 0;
-      margin: 0;
-      margin-top: 70px;
-    }
-
-    li {
-      margin-bottom: 20px;
-      color: #fff;
-      text-align: left;
-
-      @media (max-width: 768px) {
-        width: 100%;
-      }
-    }
-  }
-`;
-
-const VerticalBorder = styled.span`
-  width: 1px;
-  height: 18px;
-  background-color: #fff;
+  padding: 80px 60px;
 
   @media (max-width: 768px) {
     display: none;
   }
 `;
 
+// memo: SP_SCREENのメニュー
+const SP_SCREEN = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  position: fixed;
+  z-index: 1;
+  mix-blend-mode: normal;
+  height: 71px;
+  padding: 36px 20px;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+// memo: SP_SCREEN OPEN時のメニュー
+const SP_SCREEN_MENU = styled.div<{ $isOpen: boolean }>`
+  display: ${(props) => (props.$isOpen ? "block" : "none")};
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  z-index: 100;
+  padding: 36px 20px;
+`;
+
+// memo: 横並びにする
+const RowWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: #fff;
+  align-items: center;
+  gap: 20px;
+`;
+
+// memo: 横並びにする(間隔を空ける)
+const RowSpaceBetweenWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: #fff;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+// memo: 縦並びにする
+const ColumnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #fff;
+  align-items: center;
+  gap: 20px;
+`;
+
+// memo: SNSのアイコン
+const SNSWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  gap: 20px;
+  margin-top: 20px;
+`;
+
+// memo: ロゴ
+const Logo = styled.img`
+  width: 100%;
+  height: 100%;
+  max-width: 88px;
+  z-index: 2;
+
+  @media (max-width: 768px) {
+    max-width: 65px;
+  }
+`;
+
+const OpenWrapper = styled.div<{ $isOpen: boolean }>`
+  display: inline-block;
+  cursor: pointer;
+  position: relative;
+  width: 25px;
+  height: 20px;
+  vertical-align: middle;
+
+  & > span {
+    display: block;
+    position: absolute;
+    height: 1px;
+    width: 100%;
+    background: #fff;
+    border-radius: 9px;
+    transition: 0.3s;
+  }
+
+  & > span:nth-child(1) {
+    top: ${(props) => (props.$isOpen ? "50%" : "0")};
+    transform: ${(props) => (props.$isOpen ? "rotate(45deg)" : "none")};
+  }
+
+  & > span:nth-child(2) {
+    top: 50%;
+    transform: translateY(-50%);
+    opacity: ${(props) => (props.$isOpen ? "0" : "1")};
+  }
+
+  & > span:nth-child(3) {
+    bottom: ${(props) => (props.$isOpen ? "50%" : "0")};
+    transform: ${(props) => (props.$isOpen ? "rotate(-45deg)" : "none")};
+  }
+`;
+
+const OpenOneSpan = styled.span<{ $isOpen: boolean }>`
+  top: ${(props) => (props.$isOpen ? "50%" : "0")};
+  transform: ${(props) => (props.$isOpen ? "rotate(45deg)" : "none")};
+`;
+
+const OpenTwoSpan = styled.span<{ $isOpen: boolean }>`
+  top: 50%;
+  transform: translateY(-50%);
+  opacity: ${(props) => (props.$isOpen ? "0" : "1")};
+`;
+
+const OpenThreeSpan = styled.span<{ $isOpen: boolean }>`
+  bottom: ${(props) => (props.$isOpen ? "50%" : "0")};
+  transform: ${(props) => (props.$isOpen ? "rotate(-45deg)" : "none")};
+`;
+
+// memo: バツボタン
+const CloseButton = styled.span<{ $isOpen: boolean }>`
+  display: ${(props) => (props.$isOpen ? "inline-block" : "none")};
+  vertical-align: middle;
+  color: #fff;
+  line-height: 1;
+  width: 1em;
+  height: 0.1em;
+  background: currentColor;
+  border-radius: 0.1em;
+  position: relative;
+  transform: rotate(45deg);
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: inherit;
+    border-radius: inherit;
+    transform: rotate(90deg);
+  }
+`;
+
+// memo: 下線を引くアニメーション
 const underlineAnimation = keyframes`
   from {
     width: 0;
@@ -320,6 +343,12 @@ const underlineAnimation = keyframes`
   }
 `;
 
+// memo: メニューのテキスト
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+// memo: メニューのテキスト
 const StyledSpan = styled.span`
   color: #fff;
   text-decoration: none;
@@ -342,100 +371,5 @@ const StyledSpan = styled.span`
 
   &:hover::before {
     animation: ${underlineAnimation} 0.3s ease forwards;
-  }
-`;
-
-const StyledSpecialSpan = styled.span`
-  color: #000;
-  text-decoration: none;
-  font-size: 16px;
-
-  @media (max-width: 768px) {
-    color: #000;
-  }
-`;
-
-const MenuButton = styled.div`
-  display: none;
-  @media (max-width: 768px) {
-    display: flex;
-    width: 60px;
-    height: 60px;
-    position: relative;
-    z-index: 100;
-  }
-`;
-
-const CloseButton = styled.div`
-  display: none;
-  @media (max-width: 768px) {
-    display: flex;
-    width: 30px;
-    height: 30px;
-    position: relative;
-    z-index: 100;
-  }
-`;
-
-const CrossLine = styled.div`
-  content: "";
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 2px;
-  height: 30px;
-  background: #fff;
-  z-index: 100;
-`;
-
-const Line = styled.div`
-  width: 29px;
-  height: 2px;
-  background-color: #fff;
-  border-radius: 23px;
-  margin: 3px 0;
-`;
-
-const MobileHeadArea = styled.div<{ $isOpen: boolean }>`
-  display: none;
-  @media (max-width: 768px) {
-    display: ${({ $isOpen }) => ($isOpen ? "flex" : "none")};
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-  }
-`;
-
-const SnsWrapper = styled.div<{ $isOpen: boolean }>`
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  gap: 30px;
-  width: 100%;
-
-  display: none;
-  @media (max-width: 768px) {
-    display: ${({ $isOpen }) => ($isOpen ? "flex" : "none")};
-  }
-`;
-
-const SnsIcon = styled.img`
-  width: 100%;
-  max-width: 37px;
-  max-height: 37px;
-`;
-
-const FollowUsText = styled.p<{ $isOpen: boolean }>`
-  display: none;
-  @media (max-width: 768px) {
-    display: ${({ $isOpen }) => ($isOpen ? "flex" : "none")};
-    color: #fff;
-    text-align: center;
-    font-size: 20px;
-    font-weight: 400;
-    letter-spacing: 0.75px;
-    text-decoration-line: underline;
-    font-size: 14px;
-    white-space: nowrap;
   }
 `;
