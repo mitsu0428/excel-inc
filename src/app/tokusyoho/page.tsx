8e2774710c98bc11894a78e63ae11bb5ebc5d345
mitsu0428 <mitsuprograming@gmail.com>
2024-09-25 23:55:42 +0900

    fix: privacy-polcy, tokusyoho page font

--- src/app/tokusyoho/page.tsx
+++ src/app/tokusyoho/page.tsx
@@ -10,118 +10,116 @@ export default function Tokusyoho() {
 		<Wrap>
 			<CommentWrap>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						販売業者
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						株式会社EXCEL Inc.
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						代表責任者
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						代表取締役　樋口　絢太
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						所在地
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						〒107-0062 東京都港区南青山5-12-27 WISE512ビル 305
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						電話番号
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
-						03-6824-4903
-					</Text>
+					<Text className={Font.Font.CustomInter.className}>03-6824-4903</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						電話受付時間
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						受付時間 10:00-18:00（土日祝を除く）
 					</Text>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						※受付時間外の場合は、メールにてお問い合わせください。
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						メールアドレス
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						contact@excelinc-tokyo.com
 					</Text>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						※受付時間外の場合は、メールにてお問い合わせください。
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						サイトURL
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						https://www.excelinc-tokyo.com/
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						商品の販売価格
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						各商品ページをご参照ください。
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						商品代金以外に必要な料金
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						・配送料: 1000円
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						支払方法
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						クレジットカード決済・コンビニ決済
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						支払時期
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						・クレジットカード決済: 商品注文時にお支払いが確定します。
 					</Text>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						・コンビニ決済: 注文後3日以内にお支払いください。
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						商品の引渡またはサービス提供の時期
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						ご注文日から7営業日以内に発送いたします。
 					</Text>
 				</Comment>
 				<Comment>
-					<TitleH2_Tokusyoho className={Font.Font.CustomGotchicA1.className}>
+					<TitleH2_Tokusyoho className={Font.Font.CustomInter.className}>
 						返品・交換
 					</TitleH2_Tokusyoho>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						商品到着後14日以内に限り返品・交換が可能です。
 					</Text>
-					<Text className={Font.Font.CustomGotchicA1.className}>
+					<Text className={Font.Font.CustomInter.className}>
 						返品送料
 						商品に欠陥がある場合には当方負担、お客様のご都合による返品・交換の場合にはお客様負担となります
 					</Text>
